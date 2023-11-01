5890c6261fcff7ab746540bfc733a9f254dab3eb
Somiha Tasnim <somihatasnim@Somihas-MacBook-Pro.local>
2023-11-01 19:14:19 +0600

    update

--- config/database.config.js
+++ config/database.config.js
@@ -1,18 +1,18 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-	host: 'localhost',
-	user: 'root',
-	password: '',
-	database: 'mark_agro',
+  host: "localhost",
+  user: "root",
+  password: "12345678",
+  database: "mark_agro",
 });
 
 db.connect(function (error) {
-	if (error) {
-		console.log(error);
-	} else {
-		console.log('Database Connected!');
-	}
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Database Connected!");
+  }
 });
 
 module.exports = db;
--- controllers/admin/allProductController.js
+++ controllers/admin/allProductController.js
@@ -42,3 +42,160 @@ GROUP BY p.product_id;
     return res.status(503).json({ msg: "Internal Server Error" });
   }
 };
+
+exports.editProduct = async (req, res, next) => {
+  try {
+    const {
+      product_id,
+      product_name,
+      product_price,
+      product_details_des,
+      product_cat_id,
+      seller_id,
+      quantity,
+      variants, // An array of updated variants
+      deletedVariants, // An array of variant IDs to delete
+      images, // An array of updated image URLs
+      deletedImages, // An array of image URLs to delete
+      featured_image, // The updated featured image URL
+    } = req.body;
+
+    // 1. Update product information
+    const updateProductQuery = `
+      UPDATE products
+      SET product_name = ?,
+          product_price = ?,
+          product_details_des = ?,
+          product_cat_id = ?,
+          seller_id = ?,
+          quantity = ?
+      WHERE product_id = ?;
+    `;
+    await queryAsync(updateProductQuery, [
+      product_name,
+      product_price,
+      product_details_des,
+      product_cat_id,
+      seller_id,
+      quantity,
+      product_id,
+    ]);
+
+    // 2. Update or insert variants
+    if (variants && variants.length > 0) {
+      for (const variant of variants) {
+        const { variant_id, variant_name, price } = variant;
+
+        // Check if the variant exists, and update it if it does
+        const existingVariantQuery = `
+          SELECT variant_id FROM variant
+          WHERE variant_id = ?;
+        `;
+        const existingVariant = await queryAsync(existingVariantQuery, [
+          variant_id,
+        ]);
+
+        if (existingVariant.length > 0) {
+          // Variant exists, update it
+          const updateVariantQuery = `
+            UPDATE variant
+            SET variant_name = ?,
+                price = ?
+            WHERE variant_id = ?;
+          `;
+          await queryAsync(updateVariantQuery, [
+            variant_name,
+            price,
+            variant_id,
+          ]);
+        } else {
+          // Variant doesn't exist, insert it
+          const insertVariantQuery = `
+            INSERT INTO variant (product_id, variant_name, price)
+            VALUES (?, ?, ?);
+          `;
+          await queryAsync(insertVariantQuery, [
+            product_id,
+            variant_name,
+            price,
+          ]);
+        }
+      }
+    }
+
+    // 3. Delete variants
+    if (deletedVariants && deletedVariants.length > 0) {
+      for (const variantId of deletedVariants) {
+        const deleteVariantQuery = `
+          DELETE FROM variant
+          WHERE variant_id = ?;
+        `;
+        await queryAsync(deleteVariantQuery, [variantId]);
+      }
+    }
+
+    // 4. Update or insert images
+    if (images && images.length > 0) {
+      for (const imageUrl of images) {
+        const existingImageQuery = `
+      SELECT product_image_id FROM product_image
+      WHERE product_id = ? AND product_image_url = ?;
+    `;
+        const existingImage = await queryAsync(existingImageQuery, [
+          product_id,
+          imageUrl,
+        ]);
+
+        if (existingImage.length > 0) {
+          // Image exists, update it (if needed)
+          const updateImageQuery = `
+        UPDATE product_image
+        SET featured_image = 0  -- Update other image properties if needed
+        WHERE product_id = ? AND product_image_url = ?;
+      `;
+          await queryAsync(updateImageQuery, [product_id, imageUrl]);
+        } else {
+          // Image doesn't exist, insert it
+          const insertImageQuery = `
+        INSERT INTO product_image (product_id, product_image_url, featured_image)
+        VALUES (?, ?, 0);
+      `;
+          await queryAsync(insertImageQuery, [product_id, imageUrl]);
+        }
+      }
+    }
+
+    // 5. Delete images
+    if (deletedImages && deletedImages.length > 0) {
+      for (const imageUrl of deletedImages) {
+        const deleteImageQuery = `
+      DELETE FROM product_image
+      WHERE product_id = ? AND product_image_url = ?;
+    `;
+        await queryAsync(deleteImageQuery, [product_id, imageUrl]);
+      }
+    }
+
+    // 6. Update featured image
+    if (featured_image) {
+      const updateFeaturedImageQuery = `
+        UPDATE product_image
+        SET featured_image = 0
+        WHERE product_id = ?;
+        UPDATE product_image
+        SET featured_image = 1
+        WHERE product_id = ? AND product_image_url = ?;
+      `;
+      await queryAsync(updateFeaturedImageQuery, [
+        product_id,
+        product_id,
+        featured_image,
+      ]);
+    }
+
+    return res.status(200).json({ msg: "Product updated successfully" });
+  } catch (e) {
+    console.error(e);
+    return res.status(503).json({ msg: "Internal Server Error" });
+  }
+};
